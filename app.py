4e81e5f55f06b9b18ad7bffba39f254b79f3f6d5
kittupriyatham <kittupriyatham@gmail.com>
2022-05-31 09:33:28 +0530

    v3.3

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
-from app2 import MachineLearningModelDeployment
 
-o = MachineLearningModelDeployment()
 
 app = Flask(__name__)
 
@@ -45,25 +43,6 @@ def contact():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
-
-@app.route('/projects/MLMD')
-def mlmd():
-    return o.home()
-
-
-@app.route('/projects/MLMD', methods=['POST'])
-def predictandoutput():
-    return o.predictandoutput()
-
-
-@app.route('/projects/MLMD/dashboard')
-def dashboard():
-    return o.dashboard()
-
-
-@app.route('/projects/MLMD/about')
-def mlmd_about():
-    return o.about()
 
 
 @app.route('/projects/creo')
