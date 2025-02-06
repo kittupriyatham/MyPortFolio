78976543a2717277cac34112caa42cb454becfb6
kittupriyatham <kittu.priyatham@gmail.com>
2025-02-06 11:02:35 +0530

    Resume Auto Download

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, url_for, request
+import os
 
 
 app = Flask(__name__)
@@ -27,7 +28,34 @@ def skills():
 
 @app.route('/resume')
 def resume():
-    return render_template('resume.html')
+    resume_path = './static/pdfs/potlurikrishnapriyatham.pdf'
+    if request.args.get('download'):
+        filename = request.args.get('filename')  # Get filename from query parameter
+        # filename = None
+        if not filename:
+            return "Filename not provided for download", 400
+        try:
+            return send_file(
+                resume_path,
+                as_attachment=True,
+                download_name=filename
+            )
+        except Exception as e:
+            return f"Error sending file: {e}", 500
+    else:
+        return render_template(
+            'resume.html',
+            download_f=True,
+            download_url='resume'
+            # download_url=url_for('resume_fun', download=False, filename='potlurikrishnapriyatham.pdf')
+        )
+    # if not os.path.exists(resume_path):
+    #     return "File not found", 404  # Or handle the error gracefully
+    # try:
+    #     return render_template('resume.html')
+    # except Exception as e:
+    #     return f"Error sending file: {e}", 500
+    # return render_template('resume.html')#, send_file('./potlurikrishnapriyatham.pdf', as_attachment=True)
 
 
 @app.route('/video-resume')
@@ -62,3 +90,4 @@ def array():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run()
